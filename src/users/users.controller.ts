e2eaee4fa763e081ba4f7c500e5e8f0dd970d3b9
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2022-12-01 11:09:14 -0300

    feat: finish user crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,14 +7,22 @@ import {
   Param,
   Delete,
   UseGuards,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Roles } from '@/auth/decorators/roles.decorator';
 import { RolesGuard } from '@/auth/guards/roles.guard';
 import { Role } from '@prisma/client';
+import { User } from '@/users/entities/user.entity';
 
 @Controller('users')
 @ApiTags('Users')
@@ -25,21 +33,31 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: User })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findMany() {
-    return this.usersService.findMany();
+  @ApiQuery({
+    name: 'role',
+    type: String,
+    enum: Role,
+    required: false,
+  })
+  @ApiOkResponse({ type: User })
+  findMany(@Query('role') role?: Role) {
+    return this.usersService.findMany(role);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: User })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: User })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
